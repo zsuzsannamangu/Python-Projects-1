0e3adb9f00751a86997cac65963a24e27eccd4a9
Zsuzsanna Mangu <zsuzsannamangu@gmail.com>
2023-10-12 15:52:28 -0700

    Update encapsulation.py

--- Encapsulation/encapsulation.py
+++ Encapsulation/encapsulation.py
@@ -6,15 +6,23 @@ class Car:
     #Creating two protected attributes of the class:
     _make = "Honda"
     _model = "Civic"
-    __id= "2334kdkd"
+    __idnumber= "2334kdkd"
 
     #creating a function
     def _MakeAndModel(self):
         #this function is using the protected attributes of the Car class
         print("Make: ", self._make)
         print("Model: ", self._model)
-        print("ID: ",self.__id)
+
+    def getIdNumber(self):
+        print("ID: ",self.__idnumber)
+
+    def setIdNumber(self,idnumber):
+        self.__idnumber = idnumber
+
 
 #creating objects of the class
 obj = Car()
 obj._MakeAndModel()
+obj.getIdNumber()
+obj.setIdNumber("2334kdkd")
