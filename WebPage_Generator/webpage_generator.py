eb9789d1dc0b6ffc0fc8e244f8141768c0bcbf76
Zsuzsanna Mangu <zsuzsannamangu@gmail.com>
2023-10-23 10:18:11 -0700

    Added functionality
    Added code that displays user input on the html page.

--- WebPage_Generator/webpage_generator.py
+++ WebPage_Generator/webpage_generator.py
@@ -1,41 +1,61 @@
+
+#
+# Python:   3.11.5
+#
+# Author:   Zsuzsanna Mangu
+#
+# Purpose:  This is a program that can open an HTML file in Chrome
+#           and diplay the user input on that page
+#
+
+
 import tkinter as tk
 from tkinter import *
 import webbrowser
 import os
 
-#The webbrowser module is a python library that allows
-#you to create web documents and display them in the browser
-
-#text content should change according to user input
-
-#Your task is to create a new function within the ParentWindow class
-#that takes user input as text and then displays that custom text
-#on the generated web page.
-
-
-
-
 class ParentWindow(Frame):
     def __init__(self, master):
         Frame.__init__(self, master)
         self.master.title("Web Page Generator")
-    
-        self.btn = Button(self.master, text="Default HTML Page", width=30, height=2, command=self.defaultHTML)
-        self.btn.grid(padx=(10, 10), pady=(10, 10))
+
+        self.master.resizable(width=True, height=True)
+        self.master.geometry('{}x{}'.format(500, 300))
+
+        self.varInput = StringVar()
+
+        self.lblInput = Label(self.master, text="Your text: ", font=('Helvetica', 16), fg="black")
+        self.lblInput.grid(row=0, column=0, padx=(30,0), pady=(30,0))
+
+        # call the Entry widget to get input from the user
+        self.txtInput = Entry(self.master,text=self.varInput, font=('Helvetica', 16), fg="black")
+        self.txtInput.grid(row=0, column=1, padx=(30,0), pady=(30,0))
+
+        self.btnSubmit = Button(self.master, text="Submit", font=('Helvetica', 16), command=self.defaultHTML)
+        self.btnSubmit.grid(row=2, column=1, padx=(0,0), pady=(30,0), sticky=NE)
+
+        self.btnCancel = Button(self.master, text="Cancel", font=('Helvetica', 16), command=self.cancel)
+        self.btnCancel.grid(row=2, column=1, padx=(0,90), pady=(30,0), sticky=NE)
 
         
-    def defaultHTML(self):
-        htmlText = "Stay tuned for an amazing summer sale!"
+    def defaultHTML(self):       
+        # get() is a built in method to get user input, we store it in a variable:
+        htmlText = self.varInput.get()
         # to open/create a new html file in the write mode:
         htmlFile = open("index.html", "w")
-        htmlContent = "<html>\n<body>\n<h1>" + htmlText + "</h1>\n</body>\n</html>"
+        htmlContent = "<html>\n<body><h1>" + htmlText + "</h1>\n</body></html>"
         # writing the code into the file:
         htmlFile.write(htmlContent)
         # close the file:
         htmlFile.close()
+        # open html file in Chrome:
+        # call html file using open_new_tab()
         filename = 'file:///'+os.getcwd()+'/' + 'index.html'
         webbrowser.open_new_tab(filename)
-        print("hi")
+
+    # add a function here to close the window using the destroy() method
+    def cancel(self):
+        self.master.destroy()
 
 
 if __name__ == "__main__":
